20983cd6931a3127a1c9198b25f120399aedf1c8
Sergey Savlevich <sa.savlevich@gmail.com>
2023-08-24 14:16:30 +0700

    return createRoot

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom/client';
+import createRoot from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 // import { Provider } from 'react-redux';
 // import { store } from 'store';
@@ -7,13 +7,12 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+createRoot.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById('root')
 );
 
